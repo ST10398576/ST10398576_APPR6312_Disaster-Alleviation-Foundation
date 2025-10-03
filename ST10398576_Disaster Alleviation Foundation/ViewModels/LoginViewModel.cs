c09548500744739b5af65a5cd5d9454cb8e6b524
ST10398576 <ST10398576@rcconnect.edu.za>
2025-10-03 04:04:17 +0200

    Created ViewModels Directory at the Project level

---
+++ ST10398576_Disaster Alleviation Foundation/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ST10398576_Disaster_Alleviation_Foundation.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ ST10398576_Disaster Alleviation Foundation/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ST10398576_Disaster_Alleviation_Foundation.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Role { get; set; } = "Donor";
+    }
+}
